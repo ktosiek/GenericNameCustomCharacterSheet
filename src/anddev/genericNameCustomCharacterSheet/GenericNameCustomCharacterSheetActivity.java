fee6cfce18a263bdb99f15cb95040b40d0b2a4c0
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2012-02-18 10:02:45 +0100

    first commit

---
+++ src/anddev/genericNameCustomCharacterSheet/GenericNameCustomCharacterSheetActivity.java
@@ -0,0 +1,13 @@
+package anddev.genericNameCustomCharacterSheet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GenericNameCustomCharacterSheetActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
