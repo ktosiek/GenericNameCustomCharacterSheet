7748a72b9cc74719d45b57f59a7b6780d77ab3e4
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2012-02-18 12:01:27 +0100

    Otwieranie plików

--- src/anddev/genericNameCustomCharacterSheet/GenericNameCustomCharacterSheetActivity.java
+++ src/anddev/genericNameCustomCharacterSheet/GenericNameCustomCharacterSheetActivity.java
@@ -1,13 +1,25 @@
 package anddev.genericNameCustomCharacterSheet;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class GenericNameCustomCharacterSheetActivity extends Activity {
+	private static final String TAG = GenericNameCustomCharacterSheetActivity.class.getSimpleName();
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    	super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        FileSystemAccess a = new FileSystemAccess();
+        try {
+			Log.d(TAG, a.getTemplateFiles().toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
     }
 }
